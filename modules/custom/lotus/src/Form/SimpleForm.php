afd5b0261b36899f18713b46d66ce559657fa707
shashikanthd2702 <sam@example.com>
2018-03-30 14:45:26 +0530

    commiting all changed files

---
+++ modules/custom/lotus/src/Form/SimpleForm.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\lotus\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements a simple form.
+ */
+class SimpleForm extends FormBase {
+
+  /**
+   * Build the simple form.
+   *
+   * @param array $form
+   *   Default form array structure.
+   * @param FormStateInterface $form_state
+   *   Object containing current form state.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+	  '#maxlength' => 255,
+      '#description' => $this->t('Title must be at least 15 characters in length.'),
+      '#required' => TRUE,
+    ];
+
+    // Group submit handlers in an actions element with a key of "actions" so
+    // that it gets styled correctly, and so that other modules may add actions
+    // to the form. This is not required, but is convention.
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    // Add a submit button that handles the submission of the form.
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Getter method for Form ID.
+   *
+   * The form ID is used in implementations of hook_form_alter() to allow other
+   * modules to alter the render array built by this form controller.  it must
+   * be unique site wide. It normally starts with the providing module's name.
+   *
+   * @return string
+   *   The unique ID of the form defined by this class.
+   */
+  public function getFormId() {
+    return 'lotus_simple_form';
+  }
+
+  /**
+   * Implements form validation.
+   *
+   * The validateForm method is the default method called to validate input on
+   * a form.
+   *
+   * @param array $form
+   *   The render array of the currently built form.
+   * @param FormStateInterface $form_state
+   *   Object describing the current state of the form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $title = $form_state->getValue('title');
+    if (strlen($title) < 15) {
+      // Set an error for the form element with a key of "title".
+      $form_state->setErrorByName('title', $this->t('The title must be at least 15 characters long.'));
+    }
+  }
+
+  /**
+   * Implements a form submit handler.
+   *
+   * The submitForm method is the default method called for any submit elements.
+   *
+   * @param array $form
+   *   The render array of the currently built form.
+   * @param FormStateInterface $form_state
+   *   Object describing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /*
+     * This would normally be replaced by code that actually does something
+     * with the title.
+     */
+    $title = $form_state->getValue('title');
+    drupal_set_message($this->t('You specified a title of %title.', ['%title' => $title]));
+  }
+
+}
